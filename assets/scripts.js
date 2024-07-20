a96bc933380ebfaabd0ab4c3e6f033cfac7abbf4
zasbu <136494048+zasbu@users.noreply.github.com>
2024-07-21 08:35:19 +1000

    Create scripts.js

---
+++ assets/scripts.js
@@ -0,0 +1,19 @@
+document.getElementById('search').addEventListener('keyup', function() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById('search');
+    filter = input.value.toLowerCase();
+    table = document.querySelector('table tbody');
+    tr = table.getElementsByTagName('tr');
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName('td')[0]; // Assumes search is based on the Player column
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toLowerCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }       
+    }
+});
